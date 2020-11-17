206dbd5f05357120e14031d457cc79018fd26c82
nikka84 <72551401+nikka84@users.noreply.github.com>
2020-11-17 23:41:41 +0100

    Update install.js

--- public/js/install.js
+++ public/js/install.js
@@ -6,26 +6,28 @@ installButton.addEventListener('click', installPWA);
 
 window.addEventListener('beforeinstallprompt', saveBeforeInstallPromptEvent);
 
-function saveBeforeInstallPromptEvent (evt) {
-    deferredInstallPrompt = evt;
-    installButton.removeAttribute('hidden');
+function saveBeforeInstallPromptEvent(evt) {
+  deferredInstallPrompt = evt;
+  installButton.removeAttribute('hidden');
 }
 
-function installPWA (evt) {
-    deferredInstallPrompt.promt();
-    evt.srcElement.setAttribute('hidden', true);
-    deferredInstallPrompt.userChoice.then((choice) => {
-        if(choice.outcome==='acepted'){
-            console.log('aceptado')
+function installPWA(evt) {
+  deferredInstallPrompt.prompt();
+  evt.srcElement.setAttribute('hidden', true);
+  deferredInstallPrompt.userChoice
+      .then((choice) => {
+        if (choice.outcome === 'accepted') {
+          console.log('User accepted the A2HS prompt', choice);
         } else {
-            console.log('no aceptado')
+          console.log('User dismissed the A2HS prompt', choice);
         }
         deferredInstallPrompt = null;
-    })
+      });
 }
 
 window.addEventListener('appinstalled', logAppInstalled);
 
-function logAppInstalled (evt) {
-    console.log('App pelis instalada');
+function logAppInstalled(evt) {
+  console.log('Weather App was installed.', evt);
+
 }
